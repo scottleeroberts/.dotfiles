44be0224038c723df9c5af60c4714dd723168ad8
slrbaldwin <sroberts@baldwinaviation.com>
2016-09-23 20:21:17 -0400

    initial revision

---
+++ install/brew.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if test ! $(which brew); then
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo -e "\n\nInstalling homebrew packages..."
+echo "=============================="
+
+brew install cmake
+brew install git
+brew install git-flow
+brew install imagemagick
+brew install neovim
+brew install zsh
+brew install vim
+brew install neovim
+brew install redis
+brew install reattach-to-user-namespace
+brew install ruby
+brew install stow
+brew install tree
+brew install zsh
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install openssl
+brew install python
+brew install sqlite
+brew install tmux
+
+exit 0
