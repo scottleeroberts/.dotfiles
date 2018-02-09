ca67adac8636196563ccd832f7569403c0df2772
Scott <scott.lee.roberts@gmail.com>
2018-02-08 19:59:38 -0500

    tmux: new script to manage tmux session and windows

--- zsh/.zsh/functions/opentmux.zsh
+++ zsh/.zsh/functions/opentmux.zsh
@@ -2,11 +2,14 @@ function opentmux() {
   if [ "$TMUX" = "" ]; then
     tmux ls
     echo ""
-    echo "Enter a session number, press q to not launch tmux, or n for a new session"
+    echo "Enter a session number, press w for work, q to not launch tmux, or n for a new session"
     read "response?[#/N/q] "
     case $response in
       [qQ])
         return 1
+        ;;
+      [wW])
+        sh ~/tmux.sh
         ;;
       *)
         tmux attach -t $response || tmux new
