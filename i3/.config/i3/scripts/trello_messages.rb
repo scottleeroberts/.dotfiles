04e6061162e3ae705531dc9c14a6c03465546120
Scott Roberts <scott.lee.roberts@gmail.com>
2018-08-13 11:44:39 -0400

    i3blocks: click on block goes to directly to trello notifications

--- i3/.config/i3/scripts/trello_messages.rb
+++ i3/.config/i3/scripts/trello_messages.rb
@@ -3,24 +3,21 @@ require 'yaml'
 require 'uri'
 require 'json'
 require 'net/http'
-if ENV["BLOCK_BUTTON"].to_i == 1
-  `firefox www.trello.com`
-end
 
 CONFIG = YAML.load(File.open("#{ ENV['HOME'] }/.trello.yml"))
 member_id = CONFIG.fetch('trello').fetch('member_id')
 key = CONFIG.fetch('trello').fetch('key')
 token = CONFIG.fetch('trello').fetch('token')
+username = CONFIG.fetch('trello').fetch('username')
+
+if ENV['BLOCK_BUTTON'].to_i == 1
+  `firefox www.trello.com/"#{username}"/notifications`
+end
 
 url = URI("https://api.trello.com/1/members/#{member_id}/notifications?read_filter=unread&key=#{key}&token=#{token}")
-
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 
 response = Net::HTTP.get(url)
-
 count = JSON.parse(response).count
-
-if count > 0
-  puts count
-end
+puts count if count > 0
