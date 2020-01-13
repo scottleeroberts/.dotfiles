e6bd1438e252e8585db5fc241c76113b925857f2
Scott Roberts <scott.lee.roberts@gmail.com>
2020-01-13 11:42:18 -0500

    tmux: clean up script

--- zsh/tmux.sh
+++ zsh/tmux.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-#
-# Setup a work space called `work` with two windows
-# first window has 3 panes.
-# The first pane set at 65%, split horizontally, set to api root and running vim
-# pane 2 is split at 25% and running redis-server
-# pane 3 is set to api root and bash prompt.
-# note: `api` aliased to `cd ~/path/to/work`
-#
+
 session="work"
 
 # detach from a tmux session if in one
@@ -27,9 +20,9 @@ tmux send-keys "cd ~/Development/baldwin-web" C-m
 # create a new window
 tmux new-window -t $session -n "     "
 tmux send-keys "cd ~/Development/baldwin-web" C-m
-tmux send-keys "xrandr --output eDP1 --mode 1920x1200" C-m
 tmux send-keys "pactl load-module module-dbus-protocol" C-m
 tmux send-keys "pactl load-module module-equalizer-sink " C-m
+tmux send-keys "clear" C-m
 
 # create a new window
 tmux new-window -t $session -n "     "
