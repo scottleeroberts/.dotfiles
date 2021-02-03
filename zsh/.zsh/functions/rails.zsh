2d2a27ac499011f62e6991e6cf601476c43e5ca6
Scott Roberts <scott.lee.roberts@gmail.com>
2021-02-03 10:30:33 -0500

    zsh: fix up rails docker aliases

--- zsh/.zsh/functions/rails.zsh
+++ zsh/.zsh/functions/rails.zsh
@@ -1,7 +1,6 @@
 alias console="docker-compose run web bundle exec rails console"
 
 docker_or_local() {
-
   if [[ -f docker-compose.yml ]]; then
     eval {"docker-compose run --rm --no-deps -e SKIP_SIMPLECOV=true web /bin/bash -c \"$@\""}
   else
@@ -14,7 +13,7 @@ bu() { docker_or_local "bundle update $@" }
 
 ber() { docker_or_local "bundle exec rails $@" }
 bes() { docker_or_local "bundle exec rspec $@" }
-bep() { docker_or_local "bundle exec rake parallel:spec[$RSPEC_CORES]" }
+bep() { docker_or_local "bundle exec rake parallel:spec[$RSPEC_CORES] RAILS_ENV=test" }
 bi() { docker_or_local "bundle install $@" }
 bemi() { docker_or_local "bundle exec rails generate migration $@" }
 
@@ -23,12 +22,13 @@ mit() { docker_or_local "bundle exec rake db:migrate RAILS_ENV=test" }
 pmit() { docker_or_local "bundle exec rake parallel:migrate[$RSPEC_CORES]" }
 rollback() { docker_or_local "bundle exec rake db:rollback RAILS_ENV=development" }
 
-rrq() { docker_or_local "bundle exec rails resque:work QUEUES=*" }
+rrq() { docker_or_local "bundle exec rails resque:work QUEUE=*" }
 rrs() { docker_or_local "bundle exec rake resque:scheduler" }
 
 clean_devdb() { docker_or_local "bundle exec rake db:drop db:setup RAILS_ENV=development" }
 clean_testdb() { docker_or_local "bundle exec rake db:drop db:setup RAILS_ENV=test" }
 pclean_testdb() { docker_or_local "bundle exec rake parallel:drop[$RSPEC_CORES] parallel:setup[$RSPEC_CORES] RAILS_ENV=test" }
+alias reseed="RESEED=true docker-compose up"
 
 ya() { docker_or_local "yarn $@" }
 edit_credentials() {
