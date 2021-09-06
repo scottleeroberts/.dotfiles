45267bc280ea1be660ed544d25998692c9ea1a98
Scott Roberts <scott.lee.roberts@gmail.com>
2021-09-06 16:51:33 -0400

    docker: handle docker_or_local file in project

--- zsh/.zsh/functions/rails.zsh
+++ zsh/.zsh/functions/rails.zsh
@@ -1,10 +1,25 @@
 alias console="docker-compose run web bundle exec rails console"
 
 docker_or_local() {
+  local image=""
+  local default_image="web"
+
   if [[ -f docker-compose.yml ]]; then
-    eval {"docker-compose run --rm --no-deps -e SKIP_SIMPLECOV=true web /bin/bash -c \"$@\""}
-  else
+    if [[ -f .docker_or_local ]]; then
+      local command=$(echo $1 | cut -d' ' -f1)
+      local config=$(grep "$command=" .docker_or_local)
+      local image=$(echo $config | cut -d= -f2)
+    fi
+
+    if [[ $image == "" ]]; then
+      local image=$default_image
+    fi
+  fi
+
+  if [[ $image == "" ]]; then
     eval {"SKIP_SIMPLECOV=true $@"}
+  else
+    eval {"docker-compose run --rm --no-deps -e SKIP_SIMPLECOV=true $image /bin/bash -c \"$@\""}
   fi
 }
 
