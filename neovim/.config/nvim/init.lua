b238614a88d9651955444d476894751a0fe373c6
Scott Roberts <scott.lee.roberts@gmail.com>
2022-07-10 16:59:04 -0400

    vim: Move keymaps to lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -8,6 +8,7 @@ config = vim.env.HOME .. '/.config/nvim'
 -- Requires --
 -------------
 require "user.options"
+require "user.keymaps"
 require "user.plugins"
 
 
@@ -17,7 +18,6 @@ require "user.plugins"
 local exec = vim.api.nvim_exec
 local cmd = vim.cmd
 local g = vim.g
-local map = vim.api.nvim_set_keymap
 local o = vim.opt
 
 
@@ -25,7 +25,6 @@ local o = vim.opt
 -- Setup --
 -------------
 cmd('au FileType * setlocal ai sw=2 sts=2 et')
-cmd('let mapleader = " "')
 cmd('filetype plugin indent on')
 cmd('syntax on')
 
@@ -39,93 +38,6 @@ cmd('colorscheme nightfox')
 -------------
 -- Mappings --
 -------------
-local options = { noremap = true }
-local silent_options = { noremap = true, silent = true }
-
-map('i', 'jj', '<Esc>', options)
-map('i', ',', ',<c-g>u',  options)
-map('i', '.', '.<c-g>u',  options)
-map('i', '!', '!<c-g>u',  options)
-map('i', '?', '?<c-g>u',  options)
-
-map('n', '<CR>', ':noh<CR><CR>', options)
-map('n', 'S', 'i<cr><esc>^mwgk:silent! s/\v +$//<cr>:noh<cr>$', options) -- split line
-map('n', '<leader>d', ':bd<CR>', options)
-map('n', '<leader>q', ':q<CR>', options)
-map('n', '<leader>w', ':update<CR>', options)
-map('n', '<leader>g', '<C-]>', options)
-map('n', '<leader>.', ":call RailsOpenAltCommand(expand('%'), ':vsplit')<cr>", options)
-map('n', '<leader>o', ':vs<CR>', options)
-map('n', '<leader>i', ':sp<CR>', options)
-map('n', '<leader>z', 'zR', options)
-map('n', 'n', 'nzz', options)
-map('n', 'N', 'Nzz', options)
-map('n', 'K', "mzJx'z", options)
-map('n', '<leader>gg', ':tab new<CR>', options)
-map('n', '<leader>gn', ':tabnew %<CR>', options)
-map('n', '<leader><CR>', 'gt', options)
-map('n', '<Leader>s', ':source $MYVIMRC<CR>', options)
-map('n', '<leader><tab>', 'mtgg=G`t', options) -- format entire file
-
-map('t', 'jj', '<C-\\><C-n>', options)
-
-map('v', '<C-c>', '"+y', options)
-map('n', '<C-c>', '"+yy', options)
-map('i', '<C-c>', '<ESC>"+yyi', options)
-
-map('n', 'Y', 'y$', options)
-
-exec([[
-  nnoremap <expr> k (v:count > 5 ? "m'" . v:count: "") . 'k'
-  nnoremap <expr> j (v:count > 5 ? "m'" . v:count: "") . 'j'
-]], false)
-
-map('v', 'J', ":m '>+1<CR>gv=gv", options)
-map('v', 'K', ":m '<-2<CR>gv=gv", options)
-
---tmux
-map('n', '<c-h>', ':TmuxNavigateLeft<cr>', silent_options)
-map('n', '<c-j>', ':TmuxNavigateDown<cr>', silent_options)
-map('n', '<c-k>', ':TmuxNavigateUp<cr>', silent_options)
-map('n', '<c-l>', ':TmuxNavigateRight<cr>', silent_options)
-
---floaterm
-map('n', '<leader>tt', ':FloatermToggle<CR>', options)
-map('t', '<leader>tt', "<c-\\><C-n>:FloatermToggle<CR>", options)
-
---vim-surround
-map('n', 'ys', 'ysiw', options)
-
--- Resizing splits
-map('n', '<Up>', ':resize -2<CR>', silent_options)
-map('n', '<Down>', ':resize +2<CR>', silent_options)
-map('n', '<Left>', ':vertical resize +2<CR>', silent_options)
-map('n', '<Right>', ':vertical resize -2<CR>', silent_options)
-
---nerdtree
-map('n', '<C-n>', ':NERDTreeFind<CR>', options)
-
---file and git mappings
-map('n', '<leader>/', "<cmd>lua require'telescope.builtin'.find_files({find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", options)
-map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').grep_string({search = vim.fn.input('Grep For > ')})<CR>", options)
-map('n', '<leader>fs', '<cmd>Telescope grep_string<CR>', options)
-
-map('n', '<leader>vc', '<cmd>Telescope git_commits<CR>', {})
-map('n', '<leader>vb', '<cmd>Telescope git_bcommits<CR>', {})
-map('n', '<leader>vs', '<cmd>Telescope git_stash<CR>', options)
-map('n', '<leader>vv', '<Plug>TigBlame', {})
-map('n', '<leader>mi', "<cmd>lua require'telescope.builtin'.find_files({sorting_strategy='descending', cwd='db/migrate', find_command = {'rg', '--files', '--hidden', '-g', '!.git', '--sort=path' }})<cr>", options)
-
-map('n', '<leader>gs', '<cmd>Telescope git_status<CR>', options)
-map('n', '<leader>bb', '<cmd>Telescope buffers<CR>', options)
-map('n', '<leader>gr', '<cmd>Telescope git_branches<CR>', options)
-
---navigation qwerty vs colemak
-map('n', '<leader>lq', '<Plug>UseQwertyNavigation', options)
-map('n', '<leader>lc', '<Plug>UseColemakNavigation', options)
-
---argwrap
-map('n', '<leader>a', ':ArgWrap<CR>', silent_options)
 
 ----------------
 -- Plugins --
---
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,90 @@
+local keymap = vim.api.nvim_set_keymap
+
+local options = { noremap = true }
+local silent_options = { noremap = true, silent = true }
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", silent_options)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "local map = vim.api.nvim_set_keymap
+
+-- Keymaps
+keymap('i', 'jj', '<Esc>', options)
+keymap('i', ',', ',<c-g>u',  options)
+keymap('i', '.', '.<c-g>u',  options)
+keymap('i', '!', '!<c-g>u',  options)
+keymap('i', '?', '?<c-g>u',  options)
+
+keymap('n', '<CR>', ':noh<CR><CR>', options)
+keymap('n', 'S', 'i<cr><esc>^mwgk:silent! s/\v +$//<cr>:noh<cr>$', options) -- split line
+keymap('n', '<leader>d', ':bd<CR>', options)
+keymap('n', '<leader>q', ':q<CR>', options)
+keymap('n', '<leader>w', ':update<CR>', options)
+keymap('n', '<leader>g', '<C-]>', options)
+keymap('n', '<leader>.', ":call RailsOpenAltCommand(expand('%'), ':vsplit')<cr>", options)
+keymap('n', '<leader>o', ':vs<CR>', options)
+keymap('n', '<leader>i', ':sp<CR>', options)
+keymap('n', '<leader>z', 'zR', options)
+keymap('n', 'n', 'nzz', options)
+keymap('n', 'N', 'Nzz', options)
+keymap('n', 'K', "mzJx'z", options)
+keymap('n', '<leader>gg', ':tab new<CR>', options)
+keymap('n', '<leader>gn', ':tabnew %<CR>', options)
+keymap('n', '<leader><CR>', 'gt', options)
+keymap('n', '<Leader>s', ':source $MYVIMRC<CR>', options)
+keymap('n', '<leader><tab>', 'mtgg=G`t', options) -- format entire file
+
+keymap('t', 'jj', '<C-\\><C-n>', options)
+
+keymap('v', '<C-c>', '"+y', options)
+keymap('n', '<C-c>', '"+yy', options)
+keymap('i', '<C-c>', '<ESC>"+yyi', options)
+
+keymap('n', 'Y', 'y$', options)
+
+keymap('v', 'J', ":m '>+1<CR>gv=gv", options)
+keymap('v', 'K', ":m '<-2<CR>gv=gv", options)
+
+--tmux
+keymap('n', '<c-h>', ':TmuxNavigateLeft<cr>', silent_options)
+keymap('n', '<c-j>', ':TmuxNavigateDown<cr>', silent_options)
+keymap('n', '<c-k>', ':TmuxNavigateUp<cr>', silent_options)
+keymap('n', '<c-l>', ':TmuxNavigateRight<cr>', silent_options)
+
+--floaterm
+keymap('n', '<leader>tt', ':FloatermToggle<CR>', options)
+keymap('t', '<leader>tt', "<c-\\><C-n>:FloatermToggle<CR>", options)
+
+--vim-surround
+keymap('n', 'ys', 'ysiw', options)
+
+-- Resizing splits
+keymap('n', '<Up>', ':resize -2<CR>', silent_options)
+keymap('n', '<Down>', ':resize +2<CR>', silent_options)
+keymap('n', '<Left>', ':vertical resize +2<CR>', silent_options)
+keymap('n', '<Right>', ':vertical resize -2<CR>', silent_options)
+
+--nerdtree
+keymap('n', '<C-n>', ':NERDTreeFind<CR>', options)
+
+--file and git mappings
+keymap('n', '<leader>/', "<cmd>lua require'telescope.builtin'.find_files({find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", options)
+keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').grep_string({search = vim.fn.input('Grep For > ')})<CR>", options)
+keymap('n', '<leader>fs', '<cmd>Telescope grep_string<CR>', options)
+
+keymap('n', '<leader>vc', '<cmd>Telescope git_commits<CR>', {})
+keymap('n', '<leader>vb', '<cmd>Telescope git_bcommits<CR>', {})
+keymap('n', '<leader>vs', '<cmd>Telescope git_stash<CR>', options)
+keymap('n', '<leader>vv', '<Plug>TigBlame', {})
+keymap('n', '<leader>mi', "<cmd>lua require'telescope.builtin'.find_files({sorting_strategy='descending', cwd='db/migrate', find_command = {'rg', '--files', '--hidden', '-g', '!.git', '--sort=path' }})<cr>", options)
+
+keymap('n', '<leader>gs', '<cmd>Telescope git_status<CR>', options)
+keymap('n', '<leader>bb', '<cmd>Telescope buffers<CR>', options)
+keymap('n', '<leader>gr', '<cmd>Telescope git_branches<CR>', options)
+
+--navigation qwerty vs colemak
+keymap('n', '<leader>lq', '<Plug>UseQwertyNavigation', options)
+keymap('n', '<leader>lc', '<Plug>UseColemakNavigation', options)
+
+--argwrap
+keymap('n', '<leader>a', ':ArgWrap<CR>', silent_options)
