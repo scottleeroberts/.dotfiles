b94469d1763b02813ff176e4a73e02ab3ece916a
Scott Roberts <scott.lee.roberts@gmail.com>
2022-07-10 17:35:20 -0400

    vim: move plugin configurations to assorted.lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,7 +3,6 @@
 -------------
 config = vim.env.HOME .. '/.config/nvim'
 
-
 -------------
 -- Requires --
 -------------
@@ -14,35 +13,7 @@ require "user.theme"
 
 require "user.plugin.telescope"
 require "user.plugin.nerdtree"
-
-
--------------
--- Local  ---
--------------
-local exec = vim.api.nvim_exec
-local cmd = vim.cmd
-local g = vim.g
-local o = vim.opt
-
-
-----------------
--- Plugins --
-----------------
-
--- Quickscope
-cmd("highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline")
-cmd("highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
-
--- Statusline
-vim.api.nvim_set_option('statusline', ''
-  .. '%f '
-  .. '%m '
-  .. '%{coc#status()} '
-  .. '%r '
-  .. '%='
-  .. '%y '
-  .. '%l:%c'
-)
+require "user.plugin.assorted"
 
 
 -- Highlight current column in active pane only
@@ -54,33 +25,14 @@ vim.api.nvim_exec([[
   augroup END
 ]], false)
 
--- ruby
-g.ruby_indent_block_style = 'do' -- https://github.com/vim-ruby/vim-ruby/blob/master/doc/vim-ruby.txt#L133
-
--- auto-pairs configuration
-g.AutoPairsMultilineClose=0
-
---argwrap
-g.argwrap_padded_braces = '{'
-g.argwrap_tail_comma = 1
-
---closetag
-g.closetag_filetypes = 'html,vue,xhtml'
-g.closetag_xhtml_filetypes = 'html,vue,xhtml'
-
---coc
-g.coc_snippet_prev = '<c-p>'
-g.coc_snippet_next = '<c-n>'
-g.coc_global_extensions = { 'coc-css', 'coc-eslint', 'coc-html', 'coc-json', 'coc-snippets', 'coc-solargraph', 'coc-tsserver', 'coc-vetur', 'coc-yaml' }
-
 --iabbr
 vim.cmd('iabbr pry ::Kernel.binding.pry')
 
 -- Open to last position in file
-exec([[ au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif  ]], false)
+vim.api.nvim_exec([[ au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif  ]], false)
 
 --highlight on yank
-exec([[
+vim.api.nvim_exec([[
   au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=251, on_visual=true}
 ]], false)
 
---
+++ neovim/.config/nvim/lua/user/plugin/assorted.lua
@@ -0,0 +1,40 @@
+local exec = vim.api.nvim_exec
+local cmd = vim.cmd
+local g = vim.g
+local o = vim.opt
+
+-- Quickscope
+cmd("highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline")
+cmd("highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
+
+-- Statusline
+vim.api.nvim_set_option('statusline', ''
+  .. '%f '
+  .. '%m '
+  .. '%{coc#status()} '
+  .. '%r '
+  .. '%='
+  .. '%y '
+  .. '%l:%c'
+)
+
+-- ruby
+g.ruby_indent_block_style = 'do' -- https://github.com/vim-ruby/vim-ruby/blob/master/doc/vim-ruby.txt#L133
+
+-- auto-pairs configuration
+g.AutoPairsMultilineClose=0
+
+--argwrap
+g.argwrap_padded_braces = '{'
+g.argwrap_tail_comma = 1
+
+--closetag
+g.closetag_filetypes = 'html,vue,xhtml'
+g.closetag_xhtml_filetypes = 'html,vue,xhtml'
+
+--coc
+g.coc_snippet_prev = '<c-p>'
+g.coc_snippet_next = '<c-n>'
+g.coc_global_extensions = { 'coc-css', 'coc-eslint', 'coc-html', 'coc-json', 'coc-snippets', 'coc-solargraph', 'coc-tsserver', 'coc-vetur', 'coc-yaml' }
+
+
