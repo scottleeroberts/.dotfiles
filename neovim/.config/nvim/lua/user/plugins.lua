dc34e75b4509574122463b735bae9c770d919a79
Scott Roberts <scott.lee.roberts@gmail.com>
2022-07-10 21:20:37 -0400

    vim: Packer is in.  vim-plug is out.

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -1,49 +1,81 @@
-vim.cmd("call plug#begin('~/.config/nvim/plugged')")
+local fn = vim.fn
 
-local plugins = {
-  "Plug 'EdenEast/nightfox.nvim'",
-  "Plug 'FooSoft/vim-argwrap'",
-  "Plug 'Xuyuanp/nerdtree-git-plugin'",
-  "Plug 'airblade/vim-gitgutter'",
-  "Plug 'alvan/vim-closetag'",
-  "Plug 'ap/vim-css-color'",
-  "Plug 'benmills/vimux'",
-  "Plug 'chrisbra/Recover.vim'",
-  "Plug 'christoomey/vim-tmux-navigator'",
-  "Plug 'digitaltoad/vim-pug'",
-  "Plug 'jiangmiao/auto-pairs'",
-  "Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }",
-  "Plug 'junegunn/fzf.vim'",
-  "Plug 'kchmck/vim-coffee-script'",
-  "Plug 'kthibodeaux/tig.vim'",
-  "Plug 'kyazdani42/nvim-web-devicons'",
-  "Plug 'lisinge/vim-slim'",
-  "Plug 'neoclide/coc.nvim', {'branch': 'release'}",
-  "Plug 'nvim-lua/plenary.nvim'",
-  "Plug 'nvim-lua/popup.nvim'",
-  "Plug 'nvim-telescope/telescope-fzy-native.nvim'",
-  "Plug 'nvim-telescope/telescope.nvim'",
-  "Plug 'nvim-treesitter/nvim-treesitter'",
-  "Plug 'pangloss/vim-javascript'",
-  "Plug 'posva/vim-vue'",
-  "Plug 'ryanoasis/vim-devicons'",
-  "Plug 'scrooloose/nerdtree'",
-  "Plug 'slm-lang/vim-slm'",
-  "Plug 'thoughtbot/vim-rspec'",
-  "Plug 'tpope/vim-abolish'",
-  "Plug 'tpope/vim-endwise'",
-  "Plug 'tpope/vim-rails'",
-  "Plug 'tpope/vim-repeat'",
-  "Plug 'tpope/vim-surround'",
-  "Plug 'tyru/caw.vim'",
-  "Plug 'unblevable/quick-scope'",
-  "Plug 'vim-ruby/vim-ruby'",
-  "Plug 'voldikss/vim-floaterm'",
-  "Plug 'wellle/targets.vim'",
-}
-
-for p, plugin in ipairs(plugins) do
-  vim.cmd(plugin)
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
-vim.cmd("call plug#end()")
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "EdenEast/nightfox.nvim"
+  use "FooSoft/vim-argwrap"
+  use "Xuyuanp/nerdtree-git-plugin"
+  use "airblade/vim-gitgutter"
+  use "alvan/vim-closetag"
+  use "ap/vim-css-color"
+  use "benmills/vimux"
+  use "chrisbra/Recover.vim"
+  use "christoomey/vim-tmux-navigator"
+  use "digitaltoad/vim-pug"
+  use "jiangmiao/auto-pairs"
+  use { "junegunn/fzf", run = ":call fzf#install()" }
+  use "junegunn/fzf.vim"
+  use "kchmck/vim-coffee-script"
+  use "kthibodeaux/tig.vim"
+  use "kyazdani42/nvim-web-devicons"
+  use "lisinge/vim-slim"
+  use { 'neoclide/coc.nvim', branch = 'release' }
+  use "nvim-lua/plenary.nvim"
+  use "nvim-lua/popup.nvim"
+  use "nvim-telescope/telescope-fzy-native.nvim"
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-treesitter/nvim-treesitter"
+  use "pangloss/vim-javascript"
+  use "posva/vim-vue"
+  use "ryanoasis/vim-devicons"
+  use "scrooloose/nerdtree"
+  use "slm-lang/vim-slm"
+  use "thoughtbot/vim-rspec"
+  use "tpope/vim-abolish"
+  use "tpope/vim-endwise"
+  use "tpope/vim-rails"
+  use "tpope/vim-repeat"
+  use "tpope/vim-surround"
+  use "tyru/caw.vim"
+  use "unblevable/quick-scope"
+  use "vim-ruby/vim-ruby"
+  use "voldikss/vim-floaterm"
+  use "wellle/targets.vim"
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
