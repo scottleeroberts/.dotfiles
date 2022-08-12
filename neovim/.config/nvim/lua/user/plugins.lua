56bc1f18ea562103318ebed7350a545a403c3932
Scott Roberts <scott.lee.roberts@gmail.com>
2022-08-12 14:47:58 -0400

    vim: fix oops in plugins

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -32,7 +32,7 @@ end
 -- Install your plugins here
 return packer.startup(function(use)
   use "wbthomason/packer.nvim"
-  use "/folke/tokyonight.nvim"
+  use "folke/tokyonight.nvim"
   use "FooSoft/vim-argwrap"
   use "alvan/vim-closetag"
   use "ap/vim-css-color"
