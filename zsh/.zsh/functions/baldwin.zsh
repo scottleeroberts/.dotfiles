d1749e5dc0703407c1f4112cf3f23b06dffd3dec
Scott Roberts <scott.lee.roberts@gmail.com>
2022-08-30 13:49:16 -0400

    zsh: move `finished` to baldwin script file

--- zsh/.zsh/functions/baldwin.zsh
+++ zsh/.zsh/functions/baldwin.zsh
@@ -5,3 +5,10 @@ buildrelease() {
     echo "argument required (release name)"
   fi
 }
+
+function finished() {
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  CARD=${${BRANCH[(ws:/:)2]}[(ws:-:)1]}
+
+  wf --finish-card --card $CARD
+}
