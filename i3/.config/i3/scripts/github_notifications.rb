4e15ac8fbf9859dbe742f906237c2235efe7da66
Scott Roberts <scott.lee.roberts@gmail.com>
2023-02-08 15:16:37 -0500

    i3: code formatting

--- i3/.config/i3/scripts/github_notifications.rb
+++ i3/.config/i3/scripts/github_notifications.rb
@@ -1,14 +1,10 @@
 #!/usr/bin/env /home/sroberts/.rubies/ruby-2.7.4/bin/ruby
 require 'json'
 
-if ENV["BLOCK_BUTTON"].to_i == 1
-  `firefox https://github.com/notifications`
-end
+`firefox https://github.com/notifications` if ENV['BLOCK_BUTTON'].to_i == 1
 
-TOKEN = File.readlines("#{ ENV['HOME'] }/.github_token").first.chomp
+TOKEN = File.readlines("#{ENV['HOME']}/.github_token").first.chomp
 
-notifications = JSON.parse(`curl --silent -H "Authorization: token #{ TOKEN }" "https://api.github.com/notifications"`)
+notifications = JSON.parse(`curl --silent -H "Authorization: token #{TOKEN}" "https://api.github.com/notifications"`)
 
-if notifications.any?
-  puts notifications.size
-end
+puts notifications.size if notifications.any?
