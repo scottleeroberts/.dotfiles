a30c5b7d1483f9e2ac8375443ca27cd81f3beca1
Scott Roberts <scott.lee.roberts@gmail.com>
2023-02-14 09:31:38 -0500

    vim: no need for ensure_installed on lsp config

--- neovim/.config/nvim/lua/user/plugin/lsp.lua
+++ neovim/.config/nvim/lua/user/plugin/lsp.lua
@@ -2,12 +2,6 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-  'eslint',
-  'tsserver',
-  'solargraph',
-})
-
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
