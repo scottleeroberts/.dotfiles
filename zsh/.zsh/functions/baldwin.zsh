0813fe15bb7d8537668a235828d4a44a52837965
Scott Roberts <scott.lee.roberts@gmail.com>
2023-09-01 05:09:41 -0400

    zsh: `deploy` function to do production release

--- zsh/.zsh/functions/baldwin.zsh
+++ zsh/.zsh/functions/baldwin.zsh
@@ -16,3 +16,11 @@ function finished() {
 function next_story() {
   wf --start-card --card $(wf --list-unstarted-cards | fzf-tmux | awk '{print $1;}')
 }
+
+deploy() {
+  if [[ $# > 0 ]]; then
+    wf --deploy --list $1
+  else
+    echo "argument required (release name)"
+  fi
+}
