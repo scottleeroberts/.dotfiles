57649d4c889175796953f48919808ff0497562c7
Scott Roberts <scott.lee.roberts@gmail.com>
2023-09-08 07:03:30 -0400

    vim: fzf plugin install syntax doesn't work with dependency syntax

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -38,10 +38,8 @@ return packer.startup(function(use)
   use "benmills/vimux"
   use "chrisbra/Recover.vim"
   use "christoomey/vim-tmux-navigator"
-  use {
-      'junegunn/fzf.vim',
-      requires = { 'junegunn/fzf', run = ':call fzf#install()' }
-   }
+  use "junegunn/fzf"
+  use "junegunn/fzf.vim"
   use "kthibodeaux/tig.vim"
   use "nvim-lua/plenary.nvim"
   use "nvim-lua/popup.nvim"
