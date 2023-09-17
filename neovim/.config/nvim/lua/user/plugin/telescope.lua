60deb2ef7d86ef218bf214d28e31969654aa113c
Scott Roberts <scott.lee.roberts@gmail.com>
2023-09-17 07:28:52 -0400

    vim: simplify telescope  config

--- neovim/.config/nvim/lua/user/plugin/telescope.lua
+++ neovim/.config/nvim/lua/user/plugin/telescope.lua
@@ -1,6 +1,3 @@
-local telescope = {}
-
-
 function bcommits()
   local pickers = require('telescope.pickers')
   local finders = require('telescope.finders')
@@ -48,17 +45,9 @@ function bcommits()
   }):find()
 end
 
- require('telescope').setup{
+local telescope = {}
+require('telescope').setup{
   defaults = {
-      vimgrep_arguments = {
-        'rg',
-        '--line-number',
-        '--hidden',
-        '--column',
-        '--smart-case',
-        '--follow',
-        '--with-filename',
-      },
       layout_strategy = "flex",
       layout_config = {
         preview_cutoff = 10,
@@ -69,14 +58,5 @@ end
         },
       },
    },
-   pickers = {
-   },
-   extensions = {
-      fzy_native = {
-          override_generic_sorter = false,
-          override_file_sorter = true,
-          case_mode = "smart_case",
-      }
-   }
- }
+}
 return telescope
