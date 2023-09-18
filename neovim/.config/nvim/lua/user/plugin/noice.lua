9cf9f42e755d8a215c3607ac299833f7b32d0e50
Scott Roberts <scott.lee.roberts@gmail.com>
2023-09-18 06:19:33 -0400

    vim: customize Noice

--- neovim/.config/nvim/lua/user/plugin/noice.lua
+++ neovim/.config/nvim/lua/user/plugin/noice.lua
@@ -19,4 +19,21 @@ require("noice").setup({
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
+  views = {
+    cmdline_popup = {
+      position = { row = vim.o.lines * 0.32, col = "50%" },
+      size = { width = 60, height = "auto" },
+    },
+    popupmenu = {
+      position = { row = vim.o.lines * 0.32 + 3, col = "50%" },
+      size = { width = 60, height = 10 },
+      border = { style = "rounded", padding = { 0, 1 } },
+      win_options = {
+        winhighlight = {
+          Normal = "Normal",
+          FloatBorder = "DiagnosticInfo",
+        },
+      },
+    },
+  },
 })
