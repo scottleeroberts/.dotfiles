2460e5733b733237d589c062ce7c276cda54f053
Scott Roberts <scott.lee.roberts@gmail.com>
2023-10-20 06:19:46 -0400

    vim: use fzf inside telescope for regex search

--- neovim/.config/nvim/lua/user/plugin/telescope.lua
+++ neovim/.config/nvim/lua/user/plugin/telescope.lua
@@ -42,18 +42,15 @@ end
 
 local telescope = {}
 require('telescope').setup{
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+    }
+  },
   defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--line-number',
-      '--hidden',
-      '--column',
-      '--smart-case',
-      '--follow',
-      '--with-filename',
-      '--color=never',
-      '--no-heading',
-    },
      mappings = {
         i = {
           ['<esc>'] = 'close',
