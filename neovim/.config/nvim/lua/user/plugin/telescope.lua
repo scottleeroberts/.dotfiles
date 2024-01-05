eea2270a362241b44eab0f4d1891222f095b5609
Scott Roberts <scott.lee.roberts@gmail.com>
2024-01-05 17:09:29 -0500

    vim: set default grep command for telescope

--- neovim/.config/nvim/lua/user/plugin/telescope.lua
+++ neovim/.config/nvim/lua/user/plugin/telescope.lua
@@ -51,6 +51,16 @@ require('telescope').setup{
     }
   },
   defaults = {
+    vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+     },
      mappings = {
         i = {
           ['<esc>'] = 'close',
