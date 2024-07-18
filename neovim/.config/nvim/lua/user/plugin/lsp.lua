a52ac8c89b024c49c65c2034b34f799c6856c4bc
Scott Roberts <scott.lee.roberts@gmail.com>
2024-07-18 10:40:01 -0400

    Vim: Fix bug in lsp code

--- neovim/.config/nvim/lua/user/plugin/lsp.lua
+++ neovim/.config/nvim/lua/user/plugin/lsp.lua
@@ -10,12 +10,12 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
-lsp_zero.on_attach(function(client, bufnr)
-    lsp_zero.default_keymaps({buffer = bufnr})
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
 
     local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
     if filetype ~= 'cucumber' then
-        lsp_zero.buffer_autoformat()
+        lsp.buffer_autoformat()
     end
 end)
 lsp.setup()
