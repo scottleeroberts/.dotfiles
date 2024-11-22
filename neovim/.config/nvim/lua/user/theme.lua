89653c995acf40e42200e8778df39689cc358e57
Scott Roberts <scott.lee.roberts@gmail.com>
2024-11-22 08:18:58 -0500

    vim: color scheme tweaks for kanagawa

--- neovim/.config/nvim/lua/user/theme.lua
+++ neovim/.config/nvim/lua/user/theme.lua
@@ -17,7 +17,26 @@ require('kanagawa').setup({
               }
           }
       }
-  }
+  },
+  overrides = function(colors)
+    local theme = colors.theme
+    local makeDiagnosticColor = function(color)
+      local c = require("kanagawa.lib.color")
+      return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+    end
+
+    return {
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
+
+      DiagnosticVirtualTextHint  = makeDiagnosticColor(theme.diag.hint),
+      DiagnosticVirtualTextInfo  = makeDiagnosticColor(theme.diag.info),
+      DiagnosticVirtualTextWarn  = makeDiagnosticColor(theme.diag.warning),
+      DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+    }
+  end
 })
 
 require('nightfox').setup({
@@ -32,10 +51,16 @@ require('rose-pine').setup({
 
 require('night-owl').setup({
   disable_background = true,
+  transparent_background = true,
 })
 
 -- vim.cmd('colorscheme rose-pine')
-vim.cmd('colorscheme night-owl')
+-- vim.cmd('colorscheme night-owl')
 -- vim.cmd('colorscheme tokyonight-storm')
--- vim.cmd('colorscheme kanagawa')
+vim.cmd('colorscheme kanagawa-wave')
 -- vim.cmd('colorscheme nightfox')
+
+vim.cmd 'highlight TelescopeBorder guibg=none'
+vim.cmd 'highlight TelescopeTitle guibg=none'
+
+
