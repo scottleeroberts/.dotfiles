a8670963cd360d0013f33d28973d7d3644de0c4e
Scott Roberts <scott.lee.roberts@gmail.com>
2024-12-11 07:52:07 -0500

    vim: run EST system tests #agero

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -76,6 +76,7 @@ keymap('n', '<leader>mi', "<cmd>lua require'telescope.builtin'.find_files({sorti
 
 keymap('n', '<leader>bb', '<cmd>Telescope buffers<CR>', options)
 keymap('n', '<leader>ll', '<cmd>lua vim.diagnostic.open_float()<CR>',options)
+keymap('n', '<leader>rt', '<cmd>lua vim.fn.VimuxRunCommand("bin/eng_system_tests " .. vim.fn.expand("%:p") .. "; notify")<CR>', options)
 
 --navigation qwerty vs colemak
 keymap('n', '<leader>lq', '<Plug>UseQwertyNavigation', options)
