007370a8d60d6083184ec03604d90a5221eee349
Scott Roberts <scott.lee.roberts@gmail.com>
2025-07-18 09:08:03 -0400

    vim: keymap for telescope livegrep: `<leader>fg`

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -63,6 +63,7 @@ keymap('n', '<Right>', ':vertical resize -2<CR>', silent_options)
 keymap('n', '<leader>/', "<cmd>lua require'telescope.builtin'.find_files({find_command = {'rg', '--files', '--hidden', '-g', '!.git' }}) preview={timeout=1000}<cr>", options)
 keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').grep_string({use_regex=true, search = vim.fn.input('Grep For > '), vimgrep_arguments = {'rg', '--vimgrep', '--hidden', '--glob', '!.git/**'}})<CR>", options)
 keymap('n', '<leader>fs', "<cmd>lua require('telescope.builtin').grep_string({vimgrep_arguments = {'rg', '--vimgrep', '--hidden', '--glob', '!.git/**'}})<CR>", options)
+keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<CR>", options)
 
 keymap('n', '<leader>gc', '<cmd>Gitsigns blame_line<CR>', options)
 keymap('n', '<leader>gb', "<cmd>lua bcommits()<CR>", {})
