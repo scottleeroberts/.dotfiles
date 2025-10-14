adbdfe2b5f0b3758ac6345206a639587da3c721f
Scott Roberts <scott.lee.roberts@gmail.com>
2025-10-14 16:30:59 -0400

    vim: cleanup and optimizations to noice

--- neovim/.config/nvim/lua/user/plugin/noice.lua
+++ neovim/.config/nvim/lua/user/plugin/noice.lua
@@ -1,43 +1,42 @@
-require("noice").setup({
-  background_colour = "#222222",
-  messages = {
-    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-    -- This is a current Neovim limitation.
-    enabled = false, -- enables the Noice messages UI
-  },
-  notifications = {
-    enabled = false
-  },
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
-  },
-  views = {
-    cmdline_popup = {
-      position = { row = vim.o.lines * 0.32, col = "50%" },
-      size = { width = 60, height = "auto" },
-    },
-    popupmenu = {
-      position = { row = vim.o.lines * 0.32 + 3, col = "50%" },
-      size = { width = 60, height = 10 },
-      border = { style = "rounded", padding = { 0, 1 } },
-      win_options = {
-        winhighlight = {
-          Normal = "Normal",
-          FloatBorder = "DiagnosticInfo",
+    require("noice").setup({
+      background_colour = "#222222",
+      messages = { enabled = false },
+      notifications = { enabled = false },
+      lsp = {
+        progress = {
+          enabled = true,
+          format = "lsp_progress",
+          throttle = 1000 / 30,
+          view = "mini",
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
         },
       },
-    },
-  },
-})
+      presets = {
+        bottom_search = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+        command_palette = true,
+      },
+      views = {
+        cmdline_popup = {
+          position = { row = vim.o.lines * 0.32, col = "50%" },
+          size = { width = 60, height = "auto" },
+        },
+        popupmenu = {
+          position = { row = vim.o.lines * 0.32 + 3, col = "50%" },
+          size = { width = 60, height = 10 },
+          border = { style = "rounded", padding = { 0, 1 } },
+          win_options = {
+            winhighlight = {
+              Normal = "Normal",
+              FloatBorder = "DiagnosticInfo",
+            },
+          },
+        },
+      },
+    })
