68be9d71a5ce3baff5647de3dece22bf87c52730
Scott Roberts <scott.lee.roberts@gmail.com>
2025-10-15 08:27:14 -0400

    vim: remove redundant gitsigns config

--- neovim/.config/nvim/lua/user/config/gitsigns.lua
+++ neovim/.config/nvim/lua/user/config/gitsigns.lua
@@ -36,13 +36,4 @@ require("gitsigns").setup({
     row = 0,
     col = 1,
   },
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-  end,
 })
