287c95d6044efb4b9ae2f1734900807ffeb40c21
Scott Roberts <scott.lee.roberts@gmail.com>
2025-10-20 07:25:48 -0400

    vim: remove mux option from sidekick

--- neovim/.config/nvim/lua/user/plugins/ai.lua
+++ neovim/.config/nvim/lua/user/plugins/ai.lua
@@ -20,12 +20,6 @@ return {
   {
     "folke/sidekick.nvim",
     opts = {
-      cli = {
-        mux = {
-          backend = "tmux",
-          enabled = true,
-        },
-      },
     },
     keys = {
       {
