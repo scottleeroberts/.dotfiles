4ee05cee241212e06d1d730bd4a535d8dd5ce7ac
Scott Roberts <scott.lee.roberts@gmail.com>
2026-02-09 21:31:39 -0500

    zsh: add ppwf workflow aliases
    - Add create_ticket() for creating new Linear issues
    - Add draft() for creating draft PRs
    - Add my_prs() for viewing open PRs
    - Complements existing finished() and next_story() functions

--- zsh/.zsh/functions/ppwf.zsh
+++ zsh/.zsh/functions/ppwf.zsh
@@ -1,8 +1,20 @@
+create_ticket() {
+  ppwf --create-issue
+}
+
 finished() {
   ppwf --create-pr
 }
 
-next_story () {
+draft() {
+  ppwf --create-draft-pr
+}
+
+my_prs() {
+  ppwf --my-prs
+}
+
+next_story() {
   issue=$(ppwf --list-issues | fzf | awk '{print $1;}')
   ppwf --work-issue --issue $issue
 }
