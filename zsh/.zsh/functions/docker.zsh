7411c2ede3b5a1630473d6cfdd137b9b7fa3d072
Scott Roberts <scott.lee.roberts@gmail.com>
2026-03-19 09:38:15 -0400

    zsh: remap c to claude, move docker compose aliases to dc prefix

--- zsh/.zsh/functions/docker.zsh
+++ zsh/.zsh/functions/docker.zsh
@@ -1,10 +1,10 @@
 alias d="docker"
-alias c="docker compose"
-alias cup="docker compose up"
-alias cdn="docker compose down"
-alias cq="docker compose run web bundle exec rake resque:work QUEUE=*"
-alias cdus="docker compose down; RESEED=true docker compose up"
-alias cdu="docker compose down; docker compose up"
+alias dc="docker compose"
+alias dcup="docker compose up"
+alias dcdn="docker compose down"
+alias dcq="docker compose run web bundle exec rake resque:work QUEUE=*"
+alias dcdus="docker compose down; RESEED=true docker compose up"
+alias dcdu="docker compose down; docker compose up"
 alias cnuke="docker container prune -f; docker volume prune -f; docker image prune -f"
 
 dr_db() {
